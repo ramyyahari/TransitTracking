c86d07ac4d80ce99e61f98b4f2583c47ae2ebd1f
captain-q <quang.m.nguyen@wsu.edu>
2016-04-09 11:22:56 -0700

    added code to grab the data for Saturday routes

--- routes/index.js
+++ routes/index.js
@@ -31,5 +31,91 @@ router.get('/getBus', function(req, res) {
 
 
 
+function getSatNorth(){
+    var http = require('http');
+    var str = '';
 
+    var options = {
+        host: 'pullman.mapstrat.com',
+        path: '/nextvehicle/BusLocator.axd?&ShapeIDs=,41'//saturdayNorth
+    };
+
+    callback = function(response) {
+
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            //console.log(req.data);
+            console.log(str);
+            // your code here if you want to use the results !
+            //we should have our final strings here
+            //should save this string to route
+            if(str == "PlotBusLocations([]);")
+            {
+                console.log('no output');
+                return;
+            }
+            str += "\n"
+            fs.appendFile("BusRoutesLog/SatNorth.txt", str, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+
+                console.log("The file was saved!");
+            });
+        });
+    }
+
+//making the call, we need this here so it can do a call back
+    var req = http.request(options, callback).end();
+
+}
+
+
+function getSatSouth(){
+    var http = require('http');
+    var str = '';
+
+    var options = {
+        host: 'pullman.mapstrat.com',
+        path: '/nextvehicle/BusLocator.axd?&ShapeIDs=,42'//saturdaySouth
+    };
+
+    callback = function(response) {
+
+        response.on('data', function (chunk) {
+            str += chunk;
+        });
+
+        response.on('end', function () {
+            //console.log(req.data);
+            console.log(str);
+            // your code here if you want to use the results !
+            //we should have our final strings here
+            //should save this string to route
+            if(str == "PlotBusLocations([]);")
+            {
+                console.log('no output');
+                return;
+            }
+            str += "\n"
+            fs.appendFile("BusRoutesLog/SatSouth.txt", str, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+
+                console.log("The file was saved!");
+            });
+        });
+    }
+
+//making the call, we need this here so it can do a call back
+    var req = http.request(options, callback).end();
+
+}
+
+setInterval(getSatSouth, 5000)
+setInterval(getSatNorth, 5000)
 module.exports = router;
